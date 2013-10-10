c6edcadee3792c8fb66dc7b8f79b4135b2a3a264
jsoojeon <joonsoo@webida.org>
2013-10-10 18:44:07 +0900

    CSS-hint updated

--- addon/hint/css-hint.js
+++ addon/hint/css-hint.js
@@ -1,5 +1,10 @@
+define(['lib/codemirror/lib/codemirror', 'underscore'], function(CodeMirror, _) {
 (function () {
   "use strict";
+
+  var spec = CodeMirror.resolveMode("text/css");
+  var propertyKeywords =_.keys(spec.propertyKeywords);
+  var valueKeywords = _.keys(spec.valueKeywords).concat(_.keys(spec.colorKeywords));
 
   function getHints(cm) {
     var cur = cm.getCursor(), token = cm.getTokenAt(cur);
@@ -23,28 +28,101 @@
     if (!token.type)
       return;
 
-    var spec = CodeMirror.resolveMode("text/css");
     var keywords = null;
     if (token.type.indexOf("property") == 0)
-      keywords = spec.propertyKeywords;
+      keywords = propertyKeywords;
     else if (token.type.indexOf("variable") == 0)
-      keywords = spec.valueKeywords;
+      keywords = valueKeywords;
 
     if (!keywords)
       return;
 
-    var result = [];
-    for (var name in keywords) {
-      if (name.indexOf(token.string) == 0 /* > -1 */)
-        result.push(name);
+    function commonstring(str, sub) {
+      function calculateCommon(strP, subP) {
+        if (subP < sub.length) {
+          var n = str.substring(strP).indexOf(sub.charAt(subP));
+          if (n < 0) { return; }
+          var p = 1;
+          while (subP + p < sub.length && str.charAt(strP + n + p) === (sub.charAt(subP + p))) {
+            p++;
+          }
+          if (subP + p >= sub.length) {
+            return [{start: strP + n, len: p}];
+          } else {
+            var rest = calculateCommon(strP + n + p, subP + p);
+            if (rest) {
+              rest.push({start: strP + n, len: p});
+              return rest;
+            }
+          }
+        }
+      }
+      function calculateRelevance(common) {
+        var mcp = _.max(common, function (c) { return c.len; });
+        var rel = mcp.len - str.length - mcp.len;
+        return rel;
+      }
+      var common = calculateCommon(0, 0);
+      if (common) {
+        return (function (str, sub, common) {
+          var render = function (elem /*, self, data*/) {
+            var e = $(elem).html('');
+            function appendNormal(e, str) {
+              e.append($('<span>').text(str));
+            }
+            function appendBold(e, str) {
+              e.append($('<span class="CodeMirror-hint-common-string">').text(str));
+            }
+            var first = _.first(common);
+            if (first.start > 0) {
+              appendNormal(e, str.substring(0, first.start));
+            }
+            for (var i = 0; i < common.length; i++) {
+              var self = common[i];
+              if (i > 0) {
+                var prev = common[i - 1];
+                appendNormal(e, str.substring(prev.start + prev.len, self.start));
+              }
+              appendBold(e, str.substr(self.start, self.len));
+            }
+            var last = _.last(common);
+            if (last.start + last.len < str.length) {
+              appendNormal(e, str.substring(last.start + last.len));
+            }
+          };
+          return {
+            render: render,
+            relevance: calculateRelevance(common)
+          };
+        })(str, sub, common.reverse());
+      }
     }
-
-    return {
-      list: result,
-      from: CodeMirror.Pos(cur.line, token.start),
-      to: CodeMirror.Pos(cur.line, token.end)
-    };
+    var tstr = token.string.toLowerCase();
+    var result = _.sortBy(_.compact(_.map(keywords, function (w) {
+      var renderer = commonstring(w, tstr);
+      if (renderer) {
+        return {
+          text: w,
+          render: renderer.render,
+          relevance: renderer.relevance
+        };
+      }
+    })), function (item) { return -item.relevance; });
+    if (! _.isEmpty(result)) {
+      return {
+        list: result,
+        from: CodeMirror.Pos(cur.line, token.start),
+        to: CodeMirror.Pos(cur.line, token.end)
+      };
+    } else {
+      return {
+        list: keywords,
+        from: cur,
+        to: cur
+      };
+    }
   }
 
   CodeMirror.registerHelper("hint", "css", getHints);
 })();
+});
