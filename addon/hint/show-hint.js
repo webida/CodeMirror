e32ce7cdec177e26d2877c6170683c7496161253
Changhun Lim <changhunlim@webida.org>
2013-11-28 12:18:35 +0900

    change origin as hint-continue

--- addon/hint/show-hint.js
+++ addon/hint/show-hint.js
@@ -42,7 +42,7 @@ define(['lib/codemirror/lib/codemirror'], function(CodeMirror) {
     pick: function(data, i) {
       var completion = data.list[i];
       if (completion.hint) completion.hint(this.cm, data, completion);
-      else this.cm.replaceRange(getText(completion), data.from, data.to, "hint");
+      else this.cm.replaceRange(getText(completion), data.from, data.to, data.hintContinue ? "hint-continue" : "hint");
       this.close();
     },
 
